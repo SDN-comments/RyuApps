49c1eefcce29e4b3a9801c4f17b35645b8cc56a2
chenlundeSDN <pourmichael@gmail.com>
2015-09-09 09:05:16 +0200

    ryu apps

--- simple_spanning_tree_13.py
+++ simple_spanning_tree_13.py
@@ -1,16 +1,10 @@
 __author__ = 'mk'
-# Run the application from command line:
-# ryu-manager --observe-links simple_spanning_tree_12.py
-# Next-step: match by the datapath id
-
-import logging
-import struct
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_2
+from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
@@ -19,11 +13,11 @@ from ryu.topology.api import get_switch, get_link
 import thread
 import time
 
-class SimpleSwitch12(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_2.OFP_VERSION]
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch12, self).__init__(*args, **kwargs)
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
         self.topology_api_app = self
         self.switch_list = []
@@ -37,9 +31,10 @@ class SimpleSwitch12(app_manager.RyuApp):
         self.topo_stable = False
         self.start_learning = False
 
+
     def delete_flow(self, dpid):
         flag = 1
-        i = -1 
+        i = -1
         while i < len(self.mDataPaths):
             i += 1
             if self.mDataPaths[i].id == dpid:
@@ -60,7 +55,7 @@ class SimpleSwitch12(app_manager.RyuApp):
 
     def block_port(self, dpid, port):
         flag = 1
-        i = -1 
+        i = -1
         while i < len(self.mDataPaths):
             i += 1
             if self.mDataPaths[i].id == dpid:
@@ -74,7 +69,7 @@ class SimpleSwitch12(app_manager.RyuApp):
         match = datapath.ofproto_parser.OFPMatch(in_port=port)
         actions = []
         print 'Blocking packets from switch ', datapath.id,  ', port ', port
-        self.add_flow(datapath, match, actions)
+        self.add_flow(datapath, 1, match, actions)
 
     def enbale_port(self, dp1, port1, mac1,  dp2, port2, mac2):
         ofproto = dp1.ofproto
@@ -89,7 +84,7 @@ class SimpleSwitch12(app_manager.RyuApp):
 
         def go(count):
             time.sleep(5)
-            print 'count = %s, ev_count = %s', count, self.ev_count 
+            print 'count = %s, ev_count = %s', count, self.ev_count
             if count == self.ev_count:
                 print 'I m OK!!!'
                 self.topo_stable = True
@@ -102,7 +97,7 @@ class SimpleSwitch12(app_manager.RyuApp):
 
     def get_topology_data(self):
         if not self.topo_stable:
-            return 
+            return
         self.topo_stable = False
         print 'get_topoloty_data'
         self.switch_list = get_switch(self.topology_api_app, None)
@@ -115,7 +110,7 @@ class SimpleSwitch12(app_manager.RyuApp):
         # print 'links       : ', self.links
         print '\n\nlinks:'
         for lk in self.links:
-            print 'switch ', lk[1], ', port ', lk[3],  '--> switch ', lk[2], ', port', lk[4] 
+            print 'switch ', lk[1], ', port ', lk[3],  '--> switch ', lk[2], ', port', lk[4]
         print 'switches    : ', self.mSwitches
         self.constructing_stp_krustal()
 
@@ -129,8 +124,8 @@ class SimpleSwitch12(app_manager.RyuApp):
             dpid = block[0]
             port = block[1]
             self.block_port(dpid, port)
-       # for enable in self.ports_to_enable:
-       #     pass
+            # for enable in self.ports_to_enable:
+            #     pass
         self.start_learning = True
 
     def constructing_stp_krustal(self):
@@ -188,41 +183,68 @@ class SimpleSwitch12(app_manager.RyuApp):
             return minimum_spanning_tree
         kruskal()
 
-
-    def add_flow(self, datapath, match, actions):
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
         ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
-        inst = [datapath.ofproto_parser.OFPInstructionActions(
-            ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
 
-        mod = datapath.ofproto_parser.OFPFlowMod(
-            datapath=datapath, cookie=0, cookie_mask=0, table_id=0,
-            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-            priority=0, buffer_id=ofproto.OFP_NO_BUFFER,
-            out_port=ofproto.OFPP_ANY,
-            out_group=ofproto.OFPG_ANY,
-            flags=0, match=match, instructions=inst)
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         if not self.start_learning:
             return
+
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
         dst = eth.dst
         src = eth.src
 
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        # self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
@@ -232,25 +254,23 @@ class SimpleSwitch12(app_manager.RyuApp):
         else:
             out_port = ofproto.OFPP_FLOOD
 
-        match = datapath.ofproto_parser.OFPMatch(in_port=in_port, eth_dst=dst)
-        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+        actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
-        if self.start_learning and out_port != ofproto.OFPP_FLOOD:
+        if out_port != ofproto.OFPP_FLOOD:
             print "installing flow to avoid packet_in next time"
-            self.add_flow(datapath, match, actions)
-        else:
-            # print "self_start_learing: ", self.start_learning
-            # print "out_port          : ", out_port
-            pass
-
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
-            actions=actions, data=data)
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
-
-
